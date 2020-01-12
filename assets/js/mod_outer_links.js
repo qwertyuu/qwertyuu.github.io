9f38c9fc2f519a487e1e4779bf728ca66279ae2d
CircleCI <idkwhatismy@email.com>
2020-01-12 06:08:01 +0000

    new version Sun Jan 12 06:08:01 UTC 2020

--- assets/js/mod_outer_links.js
+++ assets/js/mod_outer_links.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var anchors = document.getElementsByTagName('a');
     var re = new RegExp(document.location.host, "g");
     for (var i = 0; i < anchors.length; i++) {
-        if (!anchors[i].href.match(re)) {
+        if (!(anchors[i].href.match(re) && anchors[i].href.includes('#'))) {
             anchors[i].setAttribute('target', '_blank');
         }
     }
