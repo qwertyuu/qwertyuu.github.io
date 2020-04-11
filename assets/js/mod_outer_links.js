c693d49119019ab6053ff24dd421730d4b907a56
CircleCI <idkwhatismy@email.com>
2020-04-11 03:38:26 +0000

    new version Sat Apr 11 03:38:26 UTC 2020

--- assets/js/mod_outer_links.js
+++ assets/js/mod_outer_links.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var anchors = document.getElementsByTagName('a');
-    var re = new RegExp(document.location.host, "g");
+    var re = new RegExp(document.location.href, "g");
     for (var i = 0; i < anchors.length; i++) {
         if (!(anchors[i].href.match(re) && anchors[i].href.includes('#'))) {
             anchors[i].setAttribute('target', '_blank');
