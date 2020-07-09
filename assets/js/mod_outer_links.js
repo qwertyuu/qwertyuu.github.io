93ebc475154977c8a434d4c29ab2e402c817ce69
CircleCI <idkwhatismy@email.com>
2020-07-09 04:13:50 +0000

    new version Thu Jul  9 04:13:50 UTC 2020

--- assets/js/mod_outer_links.js
+++ assets/js/mod_outer_links.js
@@ -1,8 +1,23 @@
+function hasAnyOf(str, includes) {
+    for (let i = 0; i < includes.length; i++) {
+        const include = includes[i];
+        if (str.includes(include)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let stayHerePlease = [
+    'raphaelcote.com/en',
+    'raphaelcote.com/fr',
+]
+
 window.onload = function () {
     var anchors = document.getElementsByTagName('a');
     var re = new RegExp(document.location.href, "g");
     for (var i = 0; i < anchors.length; i++) {
-        if (!(anchors[i].href.match(re) && anchors[i].href.includes('#'))) {
+        if (!hasAnyOf(anchors[i].href, stayHerePlease)) {
             anchors[i].setAttribute('target', '_blank');
         }
     }
